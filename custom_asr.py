90bc09de08517d2b618c0721a99a8243af6a808e
AmeyaKI <akiwalkar@berkeley.edu>
2025-12-30 13:54:35 -0800

    verifying/analyzing audio data formatting

--- custom_asr.py
+++ custom_asr.py
@@ -1,12 +1,15 @@
-from datasets import load_dataset
+from datasets import load_dataset, Audio
 import os
 from dotenv import load_dotenv
+from pathlib import Path
+import io
+import soundfile as sf
 
 load_dotenv()
 HF_TOKEN = os.getenv('HF_TOKEN')
 
 one_file = 'main/train/0000.parquet'
-cache_path = f'{os.getcwd()}/cache' 
+cache_path = f'{Path.cwd()}/cache' 
 
 dataset = load_dataset("keithito/lj_speech", 
                        revision='refs/convert/parquet',
@@ -14,15 +17,19 @@ dataset = load_dataset("keithito/lj_speech",
                        split='train',
                        cache_dir=cache_path
                        )
+dataset = dataset.cast_column("audio", Audio(decode=False)) # disable automatic decoding
 
-print(dataset)
-print(dataset['audio'])
-print(dataset['text'])
-print(dataset['normalized_text'])
+# verifying steps
+sample = next(iter(dataset)) # one file
+audio = sample['audio'] # type: ignore
+real_text = sample['text'] # type: ignore
+print(real_text)
 
-
-
-
-
-#  lj speech
+# io_audio = io.BytesIO(audio['bytes'])
+# audio_arr, sr = sf.read(io_audio)
+# sf.write("test_output.wav", audio_arr, sr)
+with open("test_output.wav", 'wb') as f:
+    f.write(audio['bytes'])
+    
+# lj speech dataset
 # https://huggingface.co/datasets/keithito/lj_speech/tree/refs%2Fconvert%2Fparquet
