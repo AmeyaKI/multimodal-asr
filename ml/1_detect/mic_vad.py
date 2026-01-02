a0855f316733de9b5c9c78ca238a572e6f56d44d
AmeyaKI <akiwalkar@berkeley.edu>
2026-01-02 14:18:40 -0800

    debugging, class edits, efficency updates

--- ml/1_detect/mic_vad.py
+++ ml/1_detect/mic_vad.py
@@ -11,19 +11,20 @@ class MicVAD():
     SAMPLE_RATE = 16000
     FRAME_SIZE = 512 
     VAD_THRESHOLD = 0.5
-
+    OUTPUT_PATH = f'{Path.home()}/Google Drive/My Drive/Colab Notebooks/assistant/vad_recordings' # Google Drive Desktop
+    
     def __init__(self, vad_model):
         self.audio_queue = Queue()
         self.vad_model = vad_model
         
-        self.FRAME_DURATION = self.FRAME_SIZE / self.SAMPLE_RATE
+        self.frame_duration = self.FRAME_SIZE / self.SAMPLE_RATE
 
         self.recorded_frames = []
         
     
-    def start_microphone(self, MAX_SILENCE=3.0, OUTPUT_PATH='../5_outputs'):
+    def start_microphone(self, max_silence=3.0, output_path=OUTPUT_PATH):
         
-        MAX_SILENT_FRAMES = int(MAX_SILENCE / self.FRAME_DURATION)
+        max_silent_frames = int(max_silence / self.frame_duration)
         print("Recording! Start speaking.")
         
         def audio_callback(indata, frames, time, status):
@@ -52,25 +53,37 @@ class MicVAD():
                         silencio_counter += 1
                         
                     # stop recording if silence max reached: 3 sec
-                    if silencio_counter >= MAX_SILENT_FRAMES:
-                        print(f"{MAX_SILENCE} seconds of silence reached. Recording stopping.")
+                    if silencio_counter >= max_silent_frames:
+                        print(f"{max_silence} seconds of silence reached. Recording stopping.")
                         break
 
             except KeyboardInterrupt: # stop recording manually
                 print("\nStopped recording.")
                 
-        self.save_recordings(OUTPUT_PATH)
+        return self.save_recordings(output_path)
 
     # save recordings to .wav file
-    def save_recordings(self, OUTPUT_PATH='../5_outputs'):
+    def save_recordings(self, output_path='../5_outputs'):
+        file_name = ''
         if self.recorded_frames:
+            # convert recorded frames to audio array
             audio = np.concatenate(self.recorded_frames)
-            audio_int16 = (audio * 32767).astype(np.int16)
-            file_num = str(len(list(Path(OUTPUT_PATH).iterdir()))).zfill(5) # long formatting...
+            audio_arr = (audio * 32767).astype(np.int16)
             
-            sci_wav.write(f"{OUTPUT_PATH}/file{file_num}.wav", self.SAMPLE_RATE, audio_int16)
-            print(f"Saved speech audio to {OUTPUT_PATH}")
+            file_num = str(len(list(Path(output_path).iterdir()))).zfill(5)
+            file_name = f"{output_path}/file{file_num}.wav"
+            
+            sci_wav.write(file_name, self.SAMPLE_RATE, audio_arr)
+            print(f"Saved speech audio to {output_path}")
+            
         else:
             print("No speech detected. No files saved")
             
+        self.recorded_frames = [] # RESOLVE LATER: delete all frames in instance of MicVAD ?
+        return file_name
+    
+
+def stitcher(): # integrate into uniform main.py file later
+    micvad = MicVAD(vad_model)
+    file_name = micvad.start_microphone() # runs microphone, saves recordings, returns file number
     
