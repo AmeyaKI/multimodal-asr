391f8c47092ec862b30b6a5f625cb31a30d9c973
AmeyaKI <akiwalkar@berkeley.edu>
2026-01-18 20:42:18 -0800

    hardcoded agentic assis pipe (v1)

---
+++ src/main_v1.py
@@ -0,0 +1,45 @@
+from mic_vad import MicVAD
+from silero_vad import load_silero_vad
+from asr_class import ASR
+from ..ml.d_agent.llm import query
+from ..ml.d_agent.agentic import execute_command, generate_applescript
+from google import genai
+from tts import TTS
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+
+def main(): 
+    # load Voice Activity Detection (VAD) model (Silero) and Mic (Sounddevice)
+    vad = load_silero_vad() # 
+    micvad_model = MicVAD(vad) 
+    
+    # load ASR (Default: Parakeet) 
+    asr_model = ASR()
+    
+    # load TTS (Default: Bark) *** replace
+    tts_model = TTS()
+    
+    # load llm (Default: Gemini)
+    client = genai.Client(api_key=API_KEY)
+    
+    # PRIMITIVE MVP
+    print("Initializing assistant. Press Ctrl+C to exit.")
+    try:
+        while True:
+            user_audio_arr = micvad_model.start_microphone() # runs microphone, saves recordings, returns file number
+            
+            user_speech_to_text = asr_model.process_arr(user_audio_arr) # translates audio arr to text
+            
+            tts_model.speak("on it")
+            applescript = generate_applescript(client, user_speech_to_text)
+            execute_command(applescript)
+            tts_model.speak("command executed")
+    except KeyboardInterrupt:
+        print("Ctrl+C pressed. Assistant terminating.")
+
+
+if __name__ == '__main__':
+    main()
